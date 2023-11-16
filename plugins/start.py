78344fef508256cae8c735f13b44af1dd2b7908a
thiccrusky <126067984+thiccrusky@users.noreply.github.com>
2023-11-16 10:11:04 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,7 +145,7 @@ async def not_joined(client: Bot, message: Message):
         ],
         [
             InlineKeyboardButton(
-                "ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ³",
+                "ᴊᴏɪɴ ɢʀᴏᴜᴘ",
                 url = client.invitelink2),
         ]
     ]
